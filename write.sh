e5f30d50ce1f0a1c8f32ca1f0a1768f677e39257
f0m41h4u7 <53977779+f0m41h4u7@users.noreply.github.com>
2019-10-21 21:38:36 +0300

    Add files via upload

--- write.sh
+++ write.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
 
-input1=$(snmpget -v2c -c Welcome 94.230.160.230 IF-MIB::ifInOctets.2 | awk -F" " '{print $NF}')
-output1=$(snmpget -v2c -c Welcome 94.230.160.230 IF-MIB::ifOutOctets.2 | awk -F" " '{print $NF}')
+input1=$(snmpwalk -v2c -c Welcome 94.230.160.230 IF-MIB::ifHCInOctets.2 | awk -F" " '{print $NF}')
+output1=$(snmpwalk -v2c -c Welcome 94.230.160.230 IF-MIB::ifHCOutOctets.2 | awk -F" " '{print $NF}')
 
 while true
 do
 	sleep 60
-	input2=$(snmpget -v2c -c Welcome 94.230.160.230 IF-MIB::ifInOctets.2 | awk -F" " '{print $NF}')
-	output2=$(snmpget -v2c -c Welcome 94.230.160.230 IF-MIB::ifOutOctets.2 | awk -F" " '{print $NF}')
+	input2=$(snmpwalk -v2c -c Welcome 94.230.160.230 IF-MIB::ifHCInOctets.2 | awk -F" " '{print $NF}')
+	output2=$(snmpwalk -v2c -c Welcome 94.230.160.230 IF-MIB::ifHCOutOctets.2 | awk -F" " '{print $NF}')
 
 	res_in=$((input2-input1))
 	res_in=$((res_in/60))
 	res_out=$((output2-output1))
         res_out=$((res_out/60))
-	#res_in=$((res_in*8))
-	#res_in=$((res_out*8))
 
 	input1=$input2
 	output1=$output2
