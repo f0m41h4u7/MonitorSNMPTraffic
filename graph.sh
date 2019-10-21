558e65f74f5e2122564777579d26053d928bc12b
f0m41h4u7 <53977779+f0m41h4u7@users.noreply.github.com>
2019-10-21 11:57:58 +0300

    Update graph.sh

--- graph.sh
+++ graph.sh
@@ -4,11 +4,6 @@ while true
 do
 sleep 60
 rrdtool graph visualize_traffic_1_hour.png --start -3600 \
-	    --full-size-mode \
-	    --slope-mode \
-            --height '300' \
-	    --rigid \
-	    --alt-autoscale \
        	    'DEF:inoctets=traffic.rrd:input:AVERAGE' \
             'DEF:outoctets=traffic.rrd:output:AVERAGE' \
 	    'CDEF:inbits=inoctets,8,*' \
@@ -17,11 +12,6 @@ rrdtool graph visualize_traffic_1_hour.png --start -3600 \
             'LINE1:inbits#0000FF:"In traffic"'
 
 rrdtool graph visualize_traffic_2_hours.png --start -7200 \
-            --full-size-mode \
-	    --slope-mode \
-            --height '300' \
-            --rigid \
-            --alt-autoscale \
             'DEF:inoctets=traffic.rrd:input:AVERAGE' \
             'DEF:outoctets=traffic.rrd:output:AVERAGE' \
             'CDEF:inbits=inoctets,8,*' \
@@ -30,11 +20,6 @@ rrdtool graph visualize_traffic_2_hours.png --start -7200 \
             'LINE1:inbits#0000FF:"In traffic"'
 
 rrdtool graph visualize_traffic_1_day.png --start -43200 \
-             --full-size-mode \
-	     --slope-mode \
-             --height '300' \
-             --rigid \
-             --alt-autoscale \
              'DEF:inoctets=traffic.rrd:input:AVERAGE' \
              'DEF:outoctets=traffic.rrd:output:AVERAGE' \
 	     'CDEF:inbits=inoctets,8,*' \
